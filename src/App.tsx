c1dd5dcc3f4cc93345b3026773283d2e8856e7f3
Martyna Markowicz <martyna@martynas-macbook-pro.home>
2019-12-01 20:59:44 +0100

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from "react";
+import { Route, Switch } from "react-router";
+import Home from "./_Components/Home/Home";
+import Map from "./_Components/Map/Map";
+import SideNavbar from "./_Routes/SideNavbar/SideNavbar";
 import "./App.css";
-import logo from "./logo.svg";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SideNavbar />
+      <Switch>
+        <Route path="/map">
+          <Map />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 };
